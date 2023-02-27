d25528c1f09ce21c787e9f8ea4ce93f5024251be
Benjamin Soddy <benjamin.soddy@gmail.com>
2023-02-27 03:41:10 -0600

    chore: updates

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -4,26 +4,31 @@ namespace HonestOverhead.Controllers
 {
     public class DefaultController : Controller
     {
+        [Route("[action]")]
         public IActionResult About()
         {
             return View();
         }
 
+        [Route("[action]")]
         public IActionResult Contact()
         {
             return View();
         }
 
+        [Route("[action]")]
         public IActionResult Error()
         {
             return View();
         }
 
+        [Route("")]
         public IActionResult Main()
         {
             return View();
         }
 
+        [Route("[action]")]
         public IActionResult Services()
         {
             return View();
