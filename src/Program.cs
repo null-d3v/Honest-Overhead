2ae1c126f201f80fb095448c204f706ce19f8790
null-d3v <benjamin.soddy@gmail.com>
2024-11-25 19:15:11 -0600

    chore: updates

--- src/Program.cs
+++ src/Program.cs
@@ -1,95 +1,68 @@
-﻿using Serilog;
-using WebMarkupMin.AspNetCore8;
+﻿using WebMarkupMin.AspNetCoreLatest;
 
-#pragma warning disable CA1031
+var webApplicationBuilder = WebApplication.CreateBuilder(args);
+webApplicationBuilder.Configuration
+    .AddJsonFile("appsettings.json", false, false)
+    .AddEnvironmentVariables();
 
-try
+webApplicationBuilder.Logging
+    .ClearProviders()
+    .AddJsonConsole();
+
+webApplicationBuilder.Services
+    .AddSingleton(webApplicationBuilder.Configuration);
+
+webApplicationBuilder.Services
+    .AddOptions();
+
+webApplicationBuilder.Services
+    .AddResponseCompression();
+webApplicationBuilder.Services
+    .AddWebOptimizer(
+        options =>
+        {
+            options.EnableDiskCache = false;
+        },
+        !webApplicationBuilder.Environment.IsDevelopment(),
+        !webApplicationBuilder.Environment.IsDevelopment());
+webApplicationBuilder.Services
+    .AddWebMarkupMin()
+    .AddHtmlMinification()
+    .AddHttpCompression();
+
+webApplicationBuilder.Services.AddRouting(
+    routeOptions => routeOptions.LowercaseUrls = true);
+
+var mvcBuilder = webApplicationBuilder.Services
+    .AddControllersWithViews();
+if (webApplicationBuilder.Environment.IsDevelopment())
 {
-    Log.Logger = new LoggerConfiguration()
-        .CreateBootstrapLogger();
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
 
-    var webApplicationBuilder = WebApplication.CreateBuilder(args);
-    webApplicationBuilder.Configuration
-        .AddJsonFile("appsettings.json", false, false)
-        .AddEnvironmentVariables();
+using var webApplication = webApplicationBuilder.Build();
 
-    webApplicationBuilder.Host.UseSerilog(
-        (context, loggerConfiguration) =>
-            loggerConfiguration
-                .ReadFrom.Configuration(
-                    context.Configuration));
-
-    webApplicationBuilder.Services
-        .AddSingleton(webApplicationBuilder.Configuration);
-
-    webApplicationBuilder.Services
-        .AddOptions();
-
-    webApplicationBuilder.Services
-        .AddResponseCompression();
-    webApplicationBuilder.Services
-        .AddWebOptimizer(
-            options =>
-            {
-                options.EnableDiskCache = false;
-            },
-            !webApplicationBuilder.Environment.IsDevelopment(),
-            !webApplicationBuilder.Environment.IsDevelopment());
-    webApplicationBuilder.Services
-        .AddWebMarkupMin()
-        .AddHtmlMinification()
-        .AddHttpCompression();
-
-    webApplicationBuilder.Services.AddRouting(
-        routeOptions => routeOptions.LowercaseUrls = true);
-
-    var mvcBuilder = webApplicationBuilder.Services
-        .AddControllersWithViews();
-    if (webApplicationBuilder.Environment.IsDevelopment())
-    {
-        mvcBuilder.AddRazorRuntimeCompilation();
-    }
-
-    using var webApplication = webApplicationBuilder.Build();
-
-    webApplication.UseSerilogRequestLogging();
-
-    if (webApplicationBuilder.Environment.IsDevelopment())
-    {
-        webApplication.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        webApplication
-            .UseExceptionHandler("/error/500");
-        webApplication
-            .UseStatusCodePagesWithReExecute("/error/{0}");
-    }
-
+if (webApplicationBuilder.Environment.IsDevelopment())
+{
+    webApplication.UseDeveloperExceptionPage();
+}
+else
+{
     webApplication
-        .UseResponseCompression()
-        .UseWebOptimizer()
-        .UseWebMarkupMin()
-        .UseStaticFiles()
-        .UseRouting();
-
-    webApplication.MapControllers();
-
-    await webApplication
-        .RunAsync()
-        .ConfigureAwait(false);
-    return 0;
-}
-catch (Exception exception)
-{
-    Log.Fatal(exception, "Host terminated unexpectedly");
-    return 1;
-}
-finally
-{
-    await Log
-        .CloseAndFlushAsync()
-        .ConfigureAwait(false);
+        .UseExceptionHandler("/error/500");
+    webApplication
+        .UseStatusCodePagesWithReExecute("/error/{0}");
 }
 
-#pragma warning restore CA1031
+webApplication
+    .UseResponseCompression()
+    .UseWebOptimizer()
+    .UseWebMarkupMin()
+    .UseStaticFiles()
+    .UseRouting();
+
+webApplication.MapControllers();
+
+await webApplication
+    .RunAsync()
+    .ConfigureAwait(false);
